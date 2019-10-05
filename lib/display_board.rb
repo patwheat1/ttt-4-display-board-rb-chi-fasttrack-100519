71b6b011c502564b8659011a31eb954878518656
patience wheatley <patience.wheatley@gmail.com>
2019-10-05 20:28:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  puts " #{board[0]}  |   |  "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "   | #{board[4]}  |   "
+  puts "    | #{board[4]} |    "
   puts "-----------"
-  puts "   |   |   "
+  puts "    |    |    "
 end 
 
 
